03a368dcd9c8eeef934bb04bba784ccdb10c8d88
Robb-Designs <rtlee@aggies.ncat.edu>
2026-06-29 01:48:27 -0400

    (server): add modular Express health route structure

--- server/src/app.js
+++ server/src/app.js
@@ -6,10 +6,13 @@ const cors = require('cors');
 
 const app = express();
 
+const healthRoutes = require('./routes/healthRoutes.js')
+
 
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use('/api/health', healthRoutes);
 
 
 // Routes
